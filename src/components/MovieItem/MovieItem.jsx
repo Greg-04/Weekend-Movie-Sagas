991b6082d19b638a8f5785811813eedbf52fc28f
Gabriel Regalado <regs0404@gmail.com>
2024-03-02 13:18:08 -0500

    added testid and test click function to images

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,8 +1,19 @@
+
 function MovieItem({ movie }) {
+
+  const handleOnClick = (event) => {
+    console.log('Test Image Click');
+  };
+
   return (
     <div data-testid="movieItem">
       <h3>{movie.title}</h3>
-      <img src={movie.poster} alt={movie.title} />
+      <img
+        src={movie.poster}
+        alt={movie.title}
+        data-testid="toDetails"
+        onClick={handleOnClick}
+      />
     </div>
   );
 }
